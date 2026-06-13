757590ea963ad41f5aa36909421f9d05b632ca91
kofoosmol <kayracebi@gmail.com>
2026-06-13 07:04:42 +0300

    Otomatik güncëlleme: 13.06.2026 07:04:42

---
+++ .config/fish/functions/ocul-dur.fish
@@ -0,0 +1,4 @@
+function ocul-dur
+    :3 create_ap --stop wlp59s0f0
+    echo "KEN kapatıldı."
+end
---
+++ .config/fish/functions/ocul.fish
@@ -0,0 +1,4 @@
+function ocul
+    :3 create_ap --daemon -c 9 wlp59s0f0 wlp59s0f0 ocul evilscientistyakub
+    echo "KEN kuruldu."
+end
